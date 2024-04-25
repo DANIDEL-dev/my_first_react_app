9c96bf96a08014e0e5ff9b08345d2d4c2ca2b98f
DANIEL OSHILI <OSHILIJRDANIEL@GMAIL.COM>
2024-04-25 15:25:44 +0100

    updated

--- src/components/ModeToggler.js
+++ src/components/ModeToggler.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import ReactPlayer from "react-player";
 export default function ModeToggler() {
-  let darkModeOn = true;
+  let darkModeOn = false;
   const darkMode = <h1>Dark Mode On</h1>;
   const lightMode = <h1>Light Mode On</h1>;
   const oya = (
     <h3>
       I did this by using React state and the onReady prop. in React
       video...Once the video loads, this prompt comes up and once you click
-      anywhere on this div, it would clear as it would return back to the
-      default state.
+      anywhere on this page, it would clear this message as it would return back
+      to the default state.
     </h3>
   );
 
@@ -19,6 +19,7 @@ export default function ModeToggler() {
     setAlertMessage(message);
   };
   function handler() {
+    darkModeOn = !darkModeOn;
     if (darkModeOn === true) {
       return darkMode;
     } else if (darkModeOn === false) {
@@ -27,12 +28,9 @@ export default function ModeToggler() {
   }
 
   return (
-    <div>
-      <div className="alert" onClick={() => setAlertMessage("")}>
-        {alertMessage}
-      </div>
-      {darkModeOn ? darkMode : lightMode}
-      <button onClick={handler}> Click Me to change display mode.</button>
+    <div className="alert" onClick={() => setAlertMessage("")}>
+      {alertMessage}
+      <button onClick={handler}> Click </button>
       <ReactPlayer
         controls
         muted={true}
